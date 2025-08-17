704053daa56c15e8064ef7b19561c89129e6bfcf
deepikaPANDU <deepikapedamalli@gmail.com>
2025-08-17 14:14:06 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main{
+    public static void main(String args[]) {
+        int a = 4;
+        for (int i = 1; i <= a; i++) {
+            int star = i;
+            int space = 2 * (a - 1);
+            for (int j = 1; j <= star; j++) {
+                System.out.println("*");
+            }
+            for (int j = 1; j <= space; j++) {
+                System.out.println(" ");
+            }
+            for (int j = 1; j <= star; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+        for (int i = a; i >= i; i--) {
+            int src = i;
+            int space = 2 * (a - 1);
+            for (int j = 1; j <= space; j++) {
+                System.out.println("*");
+            }
+            for (int j = 1; j <= space; j++) {
+                System.out.println(" ");
+            }
+            for (int j = 1; j <= star; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
